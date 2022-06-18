2a3d25351eb1995dd603dd8372384dd299ee176c
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-06-19 06:08:08 +0900

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>ああああ</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
 };
