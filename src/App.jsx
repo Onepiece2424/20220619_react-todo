215419e5b0118e31631b41021d07af5dca490ea1
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-06-25 07:45:17 +0900

    ボタンの作成

--- src/App.jsx
+++ src/App.jsx
@@ -41,14 +41,6 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
