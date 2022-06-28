64f63bab71097431c2d23b3acd50a212b2d7f4d3
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-06-28 09:37:12 +0900

    ToDo入力フォーム部分の作成

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,10 @@ export const App = () => {
 
   return (
     <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
